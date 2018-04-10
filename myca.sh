213277945243cf140258e44a9dbc60bfd09b75d2
Kaixinguo360 <kaixinguo360@gmail.com>
2018-04-10 19:46:08 +0800

    Update myca.sh

--- myca.sh
+++ myca.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
+export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+# 检查是否为Root
+[ $(id -u) != "0" ] && { echo "Error: You must be root to run this script"; exit 1; }
+
+# 检查系统信息
+if [ ! -z "`cat /etc/issue | grep 'Ubuntu 16'`" ];
+    then
+        OS='Ubuntu'
+    else
+        echo "Not support OS(Ubuntu 16), Please reinstall OS and retry!"
+        #exit 1
+fi
+
+
+## 初始化安装参数 ##
+
+# 设置静态参数
+CA_ROOT=$(dirname $(readlink -f $0))
 
 # 读取输入参数
 if [[ $1 = "-h" || $1 = "--help" || $1 = "" ]];then
-  echo -e "用法: $0 [选项]"
-  echo -e "\t-n NAME    设定名称"
-  echo -e "\t-c         新建签名证书"
+  echo -e "用法: $0 功能 [参数|选项]"
   exit 0
 fi
 
-while getopts "a:bc" arg #选项后面的冒号表示该选项需要参数
-do
-        case $arg in
-             a)
-                echo "a's arg:$OPTARG" #参数存在$OPTARG中
-                ;;
-             b)
-                echo "b"
-                ;;
-             c)
-                echo "c"
-                ;;
-             ?)  #当有不认识的选项的时候arg为?
-            echo "unkonw argument"
-        exit 1
-        ;;
-        esac
-done
+${CA_ROOT}/$1.sh $2 $3 $4 $5 $6 $7 $8 $9 $10 $11 $12 $13 $14 $15 $16

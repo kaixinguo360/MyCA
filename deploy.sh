9bc73672a3102d5a574bb728ba84bae62a0824dc
Kaixinguo360 <kaixinguo360@gmail.com>
2018-04-10 19:29:32 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,10 @@ CA_ROOT=$(dirname $(readlink -f $0))
 
 # 读取输入参数
 if [[ $1 = "-h" || $1 = "--help" || $1 = "" ]];then
-  echo "用法: $0 Password CommonName EmailAddress"
+  echo -e "用法: $0 [-n|--name Name] [-k|--key Key Path] [-c|--crt Crt Path]"
+  echo -e "\t-n --name 主机名称"
+  echo -e "\t-k --key 私钥路径"
+  echo -e "\t-c --crt 公钥路径"
   exit 0
 fi
 
@@ -31,49 +34,82 @@ if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
 eval set -- "$TEMP"
 
 while true ; do
-        case "$1" in
-                -n|--name)
-                        CommonName=$2
-                        shift 2
-                        ;;
-                -k|--key)
-                        KEY_PATH=$2
-                        shift 2
-                        ;;
-                -c|--crt)
-                        CRT_PATH=$2
-                        shift 2
-                        ;;
-                --)
-                        shift
-                        break
-                        ;;
-                *)
-                        echo "Internal error!"
-                        exit 1
-                        ;;
-        esac
+    case "$1" in
+        -n|--name)
+            CommonName=$2
+            shift 2
+            ;;
+        -k|--key)
+            KEY_PATH=$(readlink -f $2)
+            shift 2
+            ;;
+        -c|--crt)
+            CRT_PATH=$(readlink -f $2)
+            shift 2
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Internal error!"
+            exit 1
+            ;;
+    esac
 done
 
-# 非法参数
 for arg do
    echo "非法参数'$arg'" ;
    exit 1
 done
 
-echo "Continue"
+CA_ROOT=$(dirname $(readlink -f $0))
+
+## 正式安装开始 ##
+
+cd ${CA_ROOT}
+
+# 验证证书目录存在
+if [ ! -e mycerts/${CommonName} ];then
+    echo "没有找到${CommonName}证书目录！"
+    exit 1;
+fi
+cd mycerts/${CommonName}
 
 
+# 复制证书
+if [ -n "${CRT_PATH}" ];then
+    if [ -e ${CommonName}.crt ];then
+        CRT=$(< ${CommonName}.crt)
+        if [ -z "$CRT" ];then
+            echo "${CommonName}证书不合法！"
+            exit 1
+        fi
+    else
+        echo "${CommonName}证书不存在！"
+        exit 1
+    fi
 
+    mkdir -p $(dirname ${CRT_PATH}) > /dev/null
+    cp ${CommonName}.crt ${CRT_PATH}
+    echo "已将证书复制到${CRT_PATH}"
+fi
 
+# 复制密钥
+if [ -n "${KEY_PATH}" ];then
+    if [ -e ${CommonName}.key ];then
+        CRT=$(< ${CommonName}.key)
+        if [ -z "$CRT" ];then
+            echo "${CommonName}密钥不合法！"
+            exit 1
+        fi
+    else
+        echo "${CommonName}密钥不存在！"
+        exit 1
+    fi
 
-
-
-
-
-
-
-
-
-
+    mkdir -p $(dirname ${KEY_PATH}) > /dev/null
+    cp ${CommonName}.key ${KEY_PATH}
+    echo "已将密钥复制到${KEY_PATH}"
+fi
 

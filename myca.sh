6fcb87117857a46f223edd67393f01ad32b2b506
kaixinguo360 <kaixinguo360@gmail.com>
2018-10-16 23:03:05 +0800

    Create backup.sh; Modify myca.sh

--- myca.sh
+++ myca.sh
@@ -9,6 +9,7 @@ if [[ $1 = "-h" || $1 = "--help" || $1 = "" ]];then
   echo -e "用法: $0 功能 [参数|选项]"
   echo -e "    功能: issue     生成新的证书"
   echo -e "          export    导出证书"
+  echo -e "          backup    备份数据"
   exit 0
 fi
 

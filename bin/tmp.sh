08e64d151cc7dc2fc7d809be0694b1b288e334ed
Kaixinguo <kaixinguo@kaixinguo.site>
2021-01-11 11:08:23 +0800

    Update tmp.sh: add -n & -a flags

--- bin/tmp.sh
+++ bin/tmp.sh
@@ -17,6 +17,9 @@ fi
 CA_ROOT=$(realpath $(dirname $0)/..)
 CA_DATA=$CA_ROOT/data
 
+# 设置默认参数
+CommonName="tmp"
+
 # 检查是否已初始化
 [[ ! -d "${CA_DATA}/private" ]] && { echo "找不到CA根证书! 是否未初始化?"; exit 1; }
 
@@ -26,23 +29,32 @@ CA_DATA=$CA_ROOT/data
 # 读取输入参数
 if [[ $1 = "-h" || $1 = "--help" || $1 = "" ]];then
   echo "用法: $0 [-d Days -o Output]"
-  echo -e "\t-d 证书有效期(天)(默认1)"
+  echo "生成PKCS12格式的临时证书"
+  echo -e "\t-d 证书有效期(天) (默认: 1)"
+  echo -e "\t-n 主机名称 (默认: tmp)"
   echo -e "\t-p 证书保护密码"
   echo -e "\t-o 证书输出位置"
+  echo -e "\t-a 生成证书时的附加参数, 具体参阅issue命令"
   exit 0
 fi
 
-while getopts "d:p:o:" arg #选项后面的冒号表示该选项需要参数
+while getopts "d:n:p:o:a:" arg #选项后面的冒号表示该选项需要参数
 do
     case $arg in
         d)
            Days=$OPTARG
+           ;;
+        n)
+           CommonName=$OPTARG
            ;;
         p)
            Passwd=$OPTARG
            ;;
         o)
            Output=$(readlink -f $OPTARG)
+           ;;
+        a)
+           AdditionArgs="$OPTARG"
            ;;
         ?)  #当有不认识的选项的时候arg为?
            echo "未知选项"
@@ -61,12 +73,15 @@ fi
 
 # 生成证书
 cd ${CA_DATA}
-${CA_ROOT}/bin/issue.sh -n kaixinguo.tmp -d ${Days} -e kaixinguo@kaixinguo.site -f
+${CA_ROOT}/bin/issue.sh \
+    -n $CommonName \
+    -d ${Days} \
+    -f \
+    $AdditionArgs
 
 # 导出证书
-if [[ "$Passwd" == "" ]]; then
-    ${CA_ROOT}/bin/export.sh -n kaixinguo.tmp -p ${Output}
-else
-    ${CA_ROOT}/bin/export.sh -n kaixinguo.tmp -p ${Output} -o ${Passwd}
-fi
+${CA_ROOT}/bin/export.sh \
+    -n $CommonName \
+    -p ${Output} \
+    `[ -n "${Passwd}" ] && echo "-o ${Passwd}"`
 
